5d7ad96821d733fec6bd0e5099b6de12efb80043
thomfre <thomas@kaldpost.net>
2018-12-04 23:13:32 +0100

    Preparation for Day 5

---
+++ AdventOfCode2018/Solvers/Day5Solver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using JetBrains.Annotations;
+using Thomfre.AdventOfCode2018.Tools;
+
+namespace Thomfre.AdventOfCode2018.Solvers
+{
+    [UsedImplicitly]
+    internal class Day5Solver : SolverBase
+    {
+        public Day5Solver(IInputLoader inputLoader) : base(inputLoader)
+        {
+        }
+
+        public override int DayNumber => 5;
+
+        public override string Solve(ProblemPart part)
+        {
+            StartExecutionTimer();
+            string input = GetInput();
+            string[] inputSplit = input.Split('\n');
+
+            switch (part)
+            {
+                case ProblemPart.Part1:
+
+                    AnswerSolution1 = null;
+
+                    StopExecutionTimer();
+
+                    return FormatSolution($"Not solved");
+                case ProblemPart.Part2:
+
+                    AnswerSolution2 = null;
+
+                    StopExecutionTimer();
+
+                    return FormatSolution($"Not solved");
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(part), part, null);
+            }
+        }
+    }
+}
---
+++ Tests/AdventOfCode2018Tests/Solvers/Day5SolverTests.cs
@@ -0,0 +1,13 @@
+﻿using Thomfre.AdventOfCode2018.Solvers;
+
+namespace Thomfre.AdventOfCode2018.Tests.Solvers
+{
+    internal class Day5SolverTests : SolverTestBase<Day5Solver>
+    {
+        protected override string TestData1 => @"";
+        protected override string TestData2 => @"";
+
+        protected override object CorrectAnswer1 => null;
+        protected override object CorrectAnswer2 => null;
+    }
+}
