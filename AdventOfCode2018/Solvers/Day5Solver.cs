943b6b9b9cb174478d2e6c6e34e2291fde5972f1
Thomas Fredriksen <thomas@kaldpost.net>
2018-12-05 08:54:16 +0100

    Working, but slow, solution

--- AdventOfCode2018/Solvers/Day5Solver.cs
+++ AdventOfCode2018/Solvers/Day5Solver.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using JetBrains.Annotations;
 using Thomfre.AdventOfCode2018.Tools;
 
@@ -16,28 +19,77 @@ namespace Thomfre.AdventOfCode2018.Solvers
         public override string Solve(ProblemPart part)
         {
             StartExecutionTimer();
-            string input = GetInput();
-            string[] inputSplit = input.Split('\n');
+            string input = GetInput();            
 
             switch (part)
             {
                 case ProblemPart.Part1:
 
-                    AnswerSolution1 = null;
+                    int polymersRemoved = 1;
+                    while (polymersRemoved > 0)
+                    {
+                        int oldLength = input.Length;
+                        input = RemovePolymers(input);
+                        polymersRemoved = oldLength - input.Length;
+                    }
+
+                    AnswerSolution1 = input.Length;
 
                     StopExecutionTimer();
 
-                    return FormatSolution($"Not solved");
+                    return FormatSolution($"After fully removing all reactions, the resulting polymer contains [{ConsoleColor.Green}!{input.Length}] units");
                 case ProblemPart.Part2:
 
-                    AnswerSolution2 = null;
+                    char[] uniqueUnits = input.ToLower().ToCharArray().Distinct().ToArray();
+
+                    Dictionary<char, int> unitDictionary = new Dictionary<char, int>();
+
+                    foreach (char uniqueUnit in uniqueUnits)
+                    {
+                        string inputCopy = input;
+                        inputCopy = inputCopy.Replace(uniqueUnit.ToString(), "", true, CultureInfo.InvariantCulture);
+
+                        polymersRemoved = 1;
+                        while (polymersRemoved > 0)
+                        {
+                            int oldLength = inputCopy.Length;
+                            inputCopy = RemovePolymers(inputCopy);
+                            polymersRemoved = oldLength - inputCopy.Length;
+                        }
+
+                        unitDictionary.Add(uniqueUnit, inputCopy.Length);
+                    }
+
+                    KeyValuePair<char, int> bestToRemove = unitDictionary.OrderBy(u => u.Value).First();
+
+                    AnswerSolution2 = bestToRemove.Value;
 
                     StopExecutionTimer();
 
-                    return FormatSolution($"Not solved");
+                    return FormatSolution($"The best unit to remove is [{ConsoleColor.Yellow}!{char.ToLower(bestToRemove.Key)}/{char.ToUpper(bestToRemove.Key)}] giving the result [{ConsoleColor.Green}!{bestToRemove.Value}]");
                 default:
                     throw new ArgumentOutOfRangeException(nameof(part), part, null);
             }
+        }
+
+        private string RemovePolymers(string input)
+        {            
+            int i = 0;
+            while (i < input.Length - 1)
+            {
+                char current = Convert.ToChar(input.Substring(i, 1));
+                char next = Convert.ToChar(input.Substring(i+1, 1));
+                if (char.ToLower(current) == char.ToLower(next))
+                {
+                    if (char.IsUpper(current) != char.IsUpper(next))
+                    {
+                        input = input.Remove(i, 2);
+                    }
+                }
+                i++;
+            }
+
+            return input;
         }
     }
 }
--- Tests/AdventOfCode2018Tests/Solvers/Day5SolverTests.cs
+++ Tests/AdventOfCode2018Tests/Solvers/Day5SolverTests.cs
@@ -4,10 +4,10 @@ namespace Thomfre.AdventOfCode2018.Tests.Solvers
 {
     internal class Day5SolverTests : SolverTestBase<Day5Solver>
     {
-        protected override string TestData1 => @"";
-        protected override string TestData2 => @"";
+        protected override string TestData1 => "dabAcCaCBAcCcaDA";
+        protected override string TestData2 => TestData1;
 
-        protected override object CorrectAnswer1 => null;
-        protected override object CorrectAnswer2 => null;
+        protected override object CorrectAnswer1 => 10;
+        protected override object CorrectAnswer2 => 4;
     }
 }
