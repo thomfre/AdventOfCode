11894d86eb9223b4a0a6d0d09881ec8e113ef86e
Thomas Fredriksen <thomas@kaldpost.net>
2018-12-07 09:56:09 +0100

    Day 7 - not working

---
+++ AdventOfCode2018/Solvers/Day7Solver.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Thomfre.AdventOfCode2018.Tools;
+
+namespace Thomfre.AdventOfCode2018.Solvers
+{
+    internal class Day7Solver : SolverBase
+    {
+        public Day7Solver(IInputLoader inputLoader) : base(inputLoader)
+        {
+        }
+
+        public override int DayNumber => 7;
+
+        public override string Solve(ProblemPart part)
+        {
+            StartExecutionTimer();
+            string input = GetInput();
+            string[] instructions = input.Split('\n');
+
+            switch (part)
+            {
+                case ProblemPart.Part1:
+                    Dictionary<string, List<string>> dependencyDictionary = new Dictionary<string, List<string>>();
+
+                    foreach (string instruction in instructions)
+                    {
+                        MatchCollection matches = Regex.Matches(instruction, @"step (\w)", RegexOptions.IgnoreCase);
+                        string parent = matches[0].Groups[1].Value;
+                        string child = matches[1].Groups[1].Value;
+
+                        if (!dependencyDictionary.ContainsKey(child))
+                        {
+                            dependencyDictionary.Add(child, new List<string>());
+                        }
+
+                        dependencyDictionary[child].Add(parent);
+                    }
+
+                    List<string> grandParents = dependencyDictionary.Values.SelectMany(x => x)
+                                                             .Where(x => !dependencyDictionary.ContainsKey(x))
+                                                             .Distinct()
+                                                             .OrderBy(x => x)
+                                                             .ToList();
+
+                    List<string> solution = new List<string>();
+                    solution.Add(ShiftList(grandParents));
+
+                    while (dependencyDictionary.Count > 0)
+                    {
+                        (string child, List<string> parents) = dependencyDictionary.Where(x => x.Value.All(p => solution.Contains(p))).OrderBy(x => x.Key).FirstOrDefault();
+                        if (child == null)
+                        {
+                            string grandParent = ShiftList(grandParents);
+                            solution.Add(grandParent);
+                            continue;
+                        }
+
+                        dependencyDictionary.Remove(child);
+                        solution.Add(child);
+                    }
+
+                    AnswerSolution1 = string.Join("", solution);
+                    //Give BTEUFNVADWGPLRJOHMXKZQCISY, which is wrong
+
+                    StopExecutionTimer();
+
+                    return FormatSolution($"The correct order is [{ConsoleColor.Green}!{AnswerSolution1}]");
+                case ProblemPart.Part2:
+                    AnswerSolution2 = null;
+
+                    StopExecutionTimer();
+
+                    return FormatSolution($"The answer is [{ConsoleColor.Green}!{AnswerSolution2}]");
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(part), part, null);
+            }
+
+            string ShiftList(List<string> list)
+            {
+                if (list.Count == 0)
+                {
+                    return null;
+                }
+
+                string firstItem = list[0];
+                list.RemoveAt(0);
+                return firstItem;
+            }
+        }
+    }
+}
---
+++ Tests/AdventOfCode2018Tests/Solvers/Day7SolverTests.cs
@@ -0,0 +1,20 @@
+﻿using Thomfre.AdventOfCode2018.Solvers;
+
+namespace Thomfre.AdventOfCode2018.Tests.Solvers
+{
+    internal class Day7SolverTests : SolverTestBase<Day7Solver>
+    {
+        protected override string TestData1 => @"Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.";
+
+        protected override string TestData2 => TestData1;
+
+        protected override object CorrectAnswer1 => "CABDFE";
+        protected override object CorrectAnswer2 => null;
+    }
+}
